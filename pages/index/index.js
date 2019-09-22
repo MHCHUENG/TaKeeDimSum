b749f12ef020e77d9edd82be3f36f4588062c90b
MHCHUENG <mihanchueng@gmail.com>
2019-09-22 17:05:37 +0800

    feat: index page add animation

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,6 +4,113 @@ const app = getApp()
 
 Page({
   data: {
-    actionText: '馬上進入店鋪'
-  }
+    animationLogo: {},
+    animationTakee: {},
+    animationDimSum: {},
+    animationSlogan: {},
+    animationSloganB: {},
+    animationGo: {}
+  },
+
+  onShow: function() {
+    this.createGroupAnimation();
+  },
+
+  onHide: function() {
+    // this.clearAnimation();
+  },
+
+  onUnload: function () {
+    this.clearAnimation();
+  },
+
+  clearAnimation: function () {
+    clearTimeout(this.animationStepBTimer);
+    clearTimeout(this.animationStepCTimer);
+
+    this.animation.opacity(0).translateX('-200rpx').step({
+      duration: 0
+    });
+    const animationLogo = this.animation.export();
+
+    this.animation.opacity(0).translateY('-150rpx').step({
+      duration: 0
+    });
+    const animationTakee = this.animation.export();
+
+    this.animation.opacity(0).translateY('-300rpx').step({
+      duration: 0
+    });
+    const animationDimSum = this.animation.export();
+
+    this.animation.opacity(0).translateY('100rpx').step({
+      duration: 0
+    });
+    const animationSlogan = this.animation.export();
+
+    this.animation.opacity(0).translateY('150rpx').step({
+      duration: 0
+    });
+    const animationSloganB = this.animation.export();
+
+    this.animation.opacity(0).step({
+      duration: 0
+    });
+    const animationGo = this.animation.export();
+
+    this.setData({
+      animationLogo,
+      animationTakee,
+      animationDimSum,
+      animationSlogan,
+      animationSloganB,
+      animationGo
+    })
+  },
+
+  createGroupAnimation: function() {
+    var animation = this.animation ? this.animation : wx.createAnimation({
+      duration: 1000,
+      timingFunction: 'ease',
+    });
+    this.animation = animation
+
+    animation.opacity(1).translateX(0).translateY(0).step();
+    const animationStepA = animation.export();
+
+    this.setData({
+      animationLogo: animationStepA,
+      animationTakee: animationStepA,
+      animationDimSum: animationStepA,
+    })
+
+    this.animationStepBTimer = setTimeout(function() {
+      this.animation.translateY(0).opacity(0.8).step();
+      const animationStepB = this.animation.export();
+      this.setData({
+        animationSlogan: animationStepB,
+        animationSloganB: animationStepB
+      })
+    }.bind(this), 800);
+
+    this.animationStepCTimer = setTimeout(function() {
+      this.animation.opacity(1).step();
+      const animationStepC = this.animation.export();
+      this.setData({
+        animationGo: animationStepC
+      })
+    }.bind(this), 1800);
+  },
+
+  tapGoBtn: function() {
+    wx.switchTab({
+      url: '/pages/shop/shop'
+    })
+  },
+
+  tapLogo: function() {
+    wx.switchTab({
+      url: '/pages/story/story'
+    })
+  },
 })
