b87d8f7c2ff70b849f9726526eeb41cada086eec
MHCHUENG <mihanchueng@gmail.com>
2019-09-26 00:26:13 +0800

    fix: 动画

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,15 +1,7 @@
-//index.js
-//获取应用实例
-const app = getApp()
-
 Page({
   data: {
-    animationLogo: {},
-    animationTakee: {},
-    animationDimSum: {},
-    animationSlogan: {},
-    animationSloganB: {},
-    animationGo: {}
+    isAct: false,
+    stopAct: false
   },
 
   onLoad: function () {
@@ -18,105 +10,24 @@ Page({
     });
   },
 
-  onUnload: function () {
-    this.setData({
-      animationLogo: {},
-      animationTakee: {},
-      animationDimSum: {},
-      animationSlogan: {},
-      animationSloganB: {},
-      animationGo: {}
-    })
-  },
-
   onShow: function() {
-    this.createGroupAnimation();
-  },
+    if (this.timer || this.data.stopAct) {
+      return;
+    };
 
-  onHide: function() {
-    // this.clearAnimation();
-  },
-
-  onUnload: function () {
-    this.clearAnimation();
-  },
-
-  clearAnimation: function () {
-    clearTimeout(this.animationStepBTimer);
-    clearTimeout(this.animationStepCTimer);
-
-    this.animation.opacity(0).translateX('-200rpx').step({
-      duration: 0
-    });
-    const animationLogo = this.animation.export();
-
-    this.animation.opacity(0).translateY('-150rpx').step({
-      duration: 0
-    });
-    const animationTakee = this.animation.export();
-
-    this.animation.opacity(0).translateY('-300rpx').step({
-      duration: 0
-    });
-    const animationDimSum = this.animation.export();
-
-    this.animation.opacity(0).translateY('100rpx').step({
-      duration: 0
-    });
-    const animationSlogan = this.animation.export();
-
-    this.animation.opacity(0).translateY('150rpx').step({
-      duration: 0
-    });
-    const animationSloganB = this.animation.export();
-
-    this.animation.opacity(0).step({
-      duration: 0
-    });
-    const animationGo = this.animation.export();
-
-    this.setData({
-      animationLogo,
-      animationTakee,
-      animationDimSum,
-      animationSlogan,
-      animationSloganB,
-      animationGo
-    })
-  },
-
-  createGroupAnimation: function() {
-    var animation = this.animation ? this.animation : wx.createAnimation({
-      duration: 1000,
-      timingFunction: 'ease',
-    });
-    this.animation = animation
-
-    animation.opacity(1).translateX(0).translateY(0).step();
-    const animationStepA = animation.export();
-
-    this.setData({
-      animationLogo: animationStepA,
-      animationTakee: animationStepA,
-      animationDimSum: animationStepA,
-    })
-
-    this.animationStepBTimer = setTimeout(function() {
-      this.animation.translateY(0).opacity(0.8).step();
-      const animationStepB = this.animation.export();
+    this.timer = setTimeout(() => {
       this.setData({
-        animationSlogan: animationStepB,
-        animationSloganB: animationStepB
-      })
-    }.bind(this), 1000);
+        isAct: true
+      });
+    },100);
+  },
 
-    this.animationStepCTimer = setTimeout(function() {
-      this.animation.opacity(1).step();
-      const animationStepC = this.animation.export();
-      this.setData({
-        animationGo: animationStepC
-      })
-    }.bind(this), 2000);
+  onHide: function () {
+    clearTimeout(this.timer);
+    this.setData({
+      stopAct: true,
+      isAct: true
+    });
   },
 
   tapGoBtn: function() {
